f2a3e55cd7a7569755400e58e061526e72155054
Pranav K <prkrishn@hotmail.com>
2019-04-18 14:05:35 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadStream("Dir1/Hello1.txt");
+            ReadStream(@"Dir1\Hello2.txt");
+        }
+
+        private static void ReadStream(string name)
+        {
+            var stream = typeof(Program).Assembly.GetManifestResourceStream(name);
+            Console.WriteLine(new StreamReader(stream).ReadToEnd());
+        }
+    }
+}
